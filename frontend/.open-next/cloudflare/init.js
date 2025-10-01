4b69e1def96002c187738d1413f5c3fe6503a18f
ROSE16111 <qs16111@gmail.com>
2025-10-01 15:52:28 +1000

    V0.1.0

--- frontend/.open-next/cloudflare/init.js
+++ frontend/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1759294172854,
+    __BUILD_TIMESTAMP_MS__: 1759297662076,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
